83740bbcde23896c6b33d8efea5d5fe09b4aee35
Alistair Lucet <alistair.lucet@gmail.com>
2025-04-02 16:08:29 +0200

    change urls

--- users/views.py
+++ users/views.py
@@ -5,18 +5,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Compte créé avec succès !')
-            return redirect('game_home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
